460f0a0ee29a1c625514ffa53a5a0e82b1ce1493
Gary Bergman <garybergman00@gmail.com>
2020-06-05 14:57:56 -0400

    Changed alerts to confirms so user can select cancel or ok.

--- script.js
+++ script.js
@@ -50,11 +50,20 @@ var randChar = {
 // var selectNumber = document.getElementById('num');
 // var selectSymbol = document.getElementById('symb');
 
+// Basic outline for function and alerts:
+var pwChar;
+// Choose pw length and alert if not in rang
+if (pwChar > 128 || pwChar < 8) {
+  alert("Error! Password length must be 8 - 128 characters.");
+}
+
+// Choose pw character types 
+
+// if ()
 
 
 
-
-
+console.log(confirm("Include lower case?"))
 
 // Try to write a fucntion to randomize total selection based on user input:
 // function randomizer(randOne, randTwo, randThree, randFour) {
@@ -70,17 +79,15 @@ var randChar = {
 // Need to create functionality upon clicking button. 
 // Need to prompt user for length: 8-128 - prompt user to choose w/in this range if selection out of range case for to low, case for too high
 // prompt("How many characters would you like your password to contain?");
-// alert("Error! Password length must be at least 8 characters.");
-// alert("Error! Password length cannot exceed 128 characters.");
-// // Need to prompt user for special characters (symb)
-// alert("Click OK if you'd like to include special characters.");
-// // Need to prompt user for numeric
-// alert("Click OK if you'd like to include numbers.");
-// // Need to prompt user for lower
-// alert("Click OK if you'd like to inlcude lowercase letters");
-// // Need to prompt user for upper
-// alert("Click OK if you'd like to include uppercase letters.");
-// // Need to have user include at least one - prompt user to choose if none selected
+// Need to prompt user for special characters (symb)
+// confirm("Click OK if you'd like to include special characters.");
+// Need to prompt user for numeric
+// confirm("Click OK if you'd like to include numbers.");
+// Need to prompt user for lower
+// confirm("Click OK if you'd like to inlcude lowercase letters");
+// Need to prompt user for upper
+// confirm("Click OK if you'd like to include uppercase letters.");
+// Need to have user include at least one - prompt user to choose if none selected
 // alert("Error! Must include at least one character type.")
 
 // Need to generate pw derived from user choices
@@ -94,6 +101,7 @@ var randChar = {
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
