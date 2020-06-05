2fcf43a6d14d8e8e1296ae86b7ae4e8866dddbb4
Gary Bergman <garybergman00@gmail.com>
2020-06-05 18:10:24 -0400

    Deleted and reformatted code. Tried working with just strings.

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var generateBtn = document.querySelector("#generate");
 // Variables for character type:
 var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 var lower = upper.toLowerCase();
-console.log(lower)
 var num = '0123456789'
 var symb = '@%+"/\'!#$^?:,)(}{][~-_.';
 
@@ -17,20 +16,21 @@ var randSymb = symb.charAt[Math.floor(Math.random() * symb.length)]
 
 
 // User selected character types to be included in password:
-var pwCharLen;
+var pwLen;
 var includeUpper;
 var includeLower;
 var includeNumber;
 var includeSymbol;
 var selected;
-var include;
+var include = "";
+var pw = "";
 
 
 //----------------------------------------------------------------------------
 // Character type variables into key-value pairs
 var charSelect = {
-  lower: randLower,
   upper: randUpper,
+  lower: randLower,
   number: randNum,
   symbol: randSymb
 };
@@ -38,16 +38,33 @@ var charSelect = {
 //----------------------------------------------------------------------------
 
 // Choose pw length and alert if not in range
-pwCharLen = prompt("How many characters would you like your password to contain?");
-if (pwCharLen > 128 || pwCharLen < 8) {
-  alert("Error! Password length must be 8 - 128 characters.");
+function pwGen() {
+  pwLen = prompt("How many characters would you like your password to contain?");
+  if (pwLen > 128 || pwLen < 8) {
+    alert("Error! Password length must be 8 - 128 characters.");
 
-  // If pw length selection is in range, continue to character type selecton
-} else {
-  includeUpper = confirm("Click OK if you'd like to include uppercase letters.");
-  includeLower = confirm("Click OK if you'd like to inlcude lowercase letters");
-  includeNumber = confirm("Click OK if you'd like to include numbers.");
-  includeSymbol = confirm("Click OK if you'd like to include special characters.");
+    // If pw length selection is in range, continue to character type selecton
+  } else {
+    includeUpper = confirm("Click OK if you'd like to include uppercase letters.");
+    if (includeUpper) {
+      include += upper;
+    }
+
+    includeLower = confirm("Click OK if you'd like to inlcude lowercase letters");
+    if (includeLower) {
+      include += lower;
+    }
+
+    includeNumber = confirm("Click OK if you'd like to include numbers.");
+    if (includeNumber) {
+      include += number;
+    }
+
+    includeSymbol = confirm("Click OK if you'd like to include special characters.");
+    if (includeSymbol) {
+      include += symbol;
+    }
+  }
 };
 
 // If user didn't select any character types, alert goes off
@@ -57,71 +74,18 @@ if (!selected) {
   alert("Error! Must include at least one character type.")
 };
 
-// If user selects character type, add that randomized character type value to charSelect
-// if (includeUpper) {
-//   include.push(charSelect.upper);
-// };
+//Getting an error here!!
+for (i = 0; i < Len; i++) {
+  pw = include * include.length
+}
 
-// if (includeLower) {
-//   include.push(charSelect.lower);
-// };
-
-// if (includeNumber) {
-//   include.push(charSelect.number);
-// };
-
-// if (includeSymbol) {
-//   include.push(charSelect.symbol);
-// };
-
-
-// Used Fisheer-Yates Algorithm to shuffle include array
-
-// var arrayShuffle = function (include) {
-//   let newPos,
-//     temp;
-// }
-// for (let i = include.length - 1; i > 0; i--) {
-//   const j = Math.floor(Math.random() * i)
-//   const temp = inlcude[i]
-//   include[i] = include[j]
-//   inlcude[j] = temp
-// }
-
-
-console.log()
-
-// var pw;
-
-// if (pwCharLen) {
-//   for (i = 0; i < pwCharLen; i++) {
-//     pw += include[Math.floor(Math.random() * include.length)]
-//   }
-// }
-
-// console.log(pw)
-
-
-
-
-// var select = document.getElementById('selection');
-// var selectLength = document.getElementById('length');
-// var selectUpper = document.getElementById('upper');
-// var selectLower = document.getElementById('lower');
-// var selectNumber = document.getElementById('num');
-// var selectSymbol = document.getElementById('symb');
+console.log(pw)
 
 
 
 
 
 
-
-
-
-// Need to generate pw derived from user choices
-// Need to test all possibilities
-// OPTIONAL: have pw generated on page instead of in an alert
 
 
 
